7aa7e5ed9b77b3c374eff9c1f90c707ecb5f5ed1
Kunal-Sethi01 <sethikunal0108@gmail.com>
2024-12-25 17:06:52 -0500

    Added product routes and controller

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,31 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { Product } from "../models/product.model.js";
+
+const getProduct = asyncHandler(async (req, res) => {
+  const productId = req.params.id;
+
+  const product = await Product.findById(productId);
+
+  if (!product) {
+    throw new ApiError(404, "Product not found.");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, product, "Product fetched succesfully."));
+});
+
+const getAllProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find();
+
+  if (!products) {
+    throw new ApiError(400, "No existing products.");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, products, "Products list fetched."));
+});
+
+export { getProduct, getAllProducts };
