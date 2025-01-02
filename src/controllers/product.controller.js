753c5720ee9a73a146a6be2b72047db8c1950486
Kunal-Sethi01 <sethikunal0108@gmail.com>
2025-01-02 18:57:50 -0500

    Added ApiResponse and ApiError in product controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { Product } from "../models/product.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const getProduct = asyncHandler(async (req, res) => {
   const productId = req.params.id;
